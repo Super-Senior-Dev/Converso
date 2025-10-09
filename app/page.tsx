e510abcb7501c97fd922896b7cb1d9112bdc2df9
Ahmad Khan <ahmadkhanldn717@gmail.com>
2025-10-09 01:05:21 -0700

    implement dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Hello World</h1>
-      <Button>Click me</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+        id="123"
+        name="Neura the Brain Explorer"
+        topic="Neural Network of the Brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives & Integrals"
+        subject="Math"
+        duration={30}
+        color="#e5d0ff"
+        />
+        <CompanionCard
+        id="789"
+        name="Verba the Vocabulary Builder"
+        topic="language"
+        subject="English Literature"
+        duration={30}
+        color="#BDE7FF"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
